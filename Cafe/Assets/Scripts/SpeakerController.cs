e3c2479cbe9277aec33806fb8cef17e854ce060d
Adrian S <adrian.r.sanpedro@gmail.com>
2018-11-04 15:39:03 -0800

    Added textures, speaker for music

---
+++ Cafe/Assets/Scripts/SpeakerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakerController : MonoBehaviour {
+
+    public AudioClip firstClip;
+
+    private AudioSource musicPlayer;
+    
+	// Use this for initialization
+	void Start () {
+        musicPlayer = GetComponent<AudioSource>();
+        musicPlayer.clip = firstClip;
+        musicPlayer.loop = true;
+        musicPlayer.Play();
+        //StartCoroutine(WaitForTrackTOend());
+    }
+
+    /*IEnumerator WaitForTrackTOend()
+    {
+        while (musicPlayer.isPlaying)
+        {
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        musicPlayer.clip = firstClip;
+        musPlayer.loop = true;
+        MpPlayer.Play();
+
+    }*/
+}
