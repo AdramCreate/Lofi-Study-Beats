c835aad28190902817ded1b44bf7adbf6e1be715
Adrian S <adrian.r.sanpedro@gmail.com>
2018-11-04 14:25:43 -0800

    Initial VR HMD Setup

---
+++ Cafe/Assets/Scripts/TableScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Physics.IgnoreLayerCollision(9,9);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
