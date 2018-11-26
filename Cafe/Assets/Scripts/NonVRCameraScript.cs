c8916c3dc9a16200f4fbea0ade1fc43e9432dc16
Adrian S <adrian.r.sanpedro@gmail.com>
2018-11-25 19:45:45 -0800

    Added Non-VR Main Camera and Script

---
+++ Cafe/Assets/Scripts/NonVRCameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonVRCameraScript : MonoBehaviour {
+
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    private Vector2 mouseLook;
+    private Vector2 smoothV;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseDelta.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseDelta.y, 1f / smoothing);
+        mouseLook += smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+    }
+}
