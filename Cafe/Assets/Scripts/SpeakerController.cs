72e5e671b1ec553870471f281542e8546a27adb4
Adrian S <adrian.r.sanpedro@gmail.com>
2018-11-25 17:31:49 -0800

    Edited speaker code

--- Cafe/Assets/Scripts/SpeakerController.cs
+++ Cafe/Assets/Scripts/SpeakerController.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class SpeakerController : MonoBehaviour {
 
-    public AudioClip firstClip;
+    //public AudioClip firstClip;
     public OVRGrabber leftController;
     public OVRGrabber rightController;
+
+    private Object[] music;
 
     private AudioSource musicPlayer;
     private bool isVRGrabbed;
     private OVRGrabbable speakerGrabbableScript;
-    
-	// Use this for initialization
-	void Start () {
-        speakerGrabbableScript = GetComponent<OVRGrabbable>();
+
+    private void Awake()
+    {
         musicPlayer = GetComponent<AudioSource>();
-        musicPlayer.clip = firstClip;
+        music = Resources.LoadAll("Sounds/Music", typeof(AudioClip));
+        musicPlayer.clip = music[0] as AudioClip;
+    }
+    // Use this for initialization
+    void Start () {
+        
+
+        speakerGrabbableScript = GetComponent<OVRGrabbable>();
+        
+        //musicPlayer.clip = firstClip;4
         musicPlayer.loop = true;
         musicPlayer.Play();
     }
